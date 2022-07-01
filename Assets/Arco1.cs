65452923e38f09595796c45a78c307b7fa05a7c0
Maiaguiter <101569051+Maiaguiter@users.noreply.github.com>
2022-06-30 23:20:24 -0300

    Initial commit

---
+++ Assets/Arco1.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Arco1 : MonoBehaviour
+{
+    public GameObject Sphere;
+    public GameObject CubePrefab;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    void OnCollisionEnter(Collision col)
+
+    {
+        if (col.gameObject.name == "Arco")
+        {
+            Debug.Log("ded");
+            Destroy(Sphere);
+        }
+        if (col.gameObject.name == "Sphere")
+        {
+            GameObject cloncubo;
+            for (int i = 0; i < 10; i++)
+            {
+
+                cloncubo = Instantiate(CubePrefab);
+                Destroy(cloncubo, 5);
+
+            }
+
+
+        }
+
+    }
+}
---
+++ Assets/Menuingame.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Menuingame : MonoBehaviour
+{
+    public GameObject canvas;
+    public TextMeshProUGUI parrafo;
+
+    public void ShowMenu(string p)
+    {
+        canvas.SetActive(!canvas.activeSelf);
+        parrafo.text = p;
+    }
+}
